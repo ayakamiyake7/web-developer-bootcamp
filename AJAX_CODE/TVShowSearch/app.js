d232f196bb8c0c508cf90bffef82d9b202b5efc9
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-06-18 17:10:49 +0100

    28 AJAX and API's

--- AJAX_CODE/TVShowSearch/app.js
+++ AJAX_CODE/TVShowSearch/app.js
@@ -1,19 +1,41 @@
-const form = document.querySelector('#searchForm');
-form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const searchTerm = form.elements.query.value;
-    const config = { params: { q: searchTerm } }
-    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
-    makeImages(res.data)
-    form.elements.query.value = '';
-})
+// const form = document.querySelector('#searchForm');
+// form.addEventListener('submit', async function (e) {
+//     e.preventDefault();
+//     const searchTerm = form.elements.query.value;
+//     const config = { params: { q: searchTerm } }
+//     const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+//     makeImages(res.data)
+//     form.elements.query.value = '';
+// })
+
+// const makeImages = (shows) => {
+//     for (let result of shows) {
+//         if (result.show.image) {
+//             const img = document.createElement('IMG');
+//             img.src = result.show.image.medium;
+//             document.body.append(img)
+//         }
+//     }
+// }
+
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  //   console.log(form.elements.query.value);
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  //   console.log(res.data[0].show.image.medium);
+  makeImages(res.data);
+});
 
 const makeImages = (shows) => {
-    for (let result of shows) {
-        if (result.show.image) {
-            const img = document.createElement('IMG');
-            img.src = result.show.image.medium;
-            document.body.append(img)
-        }
+  for (let result of shows) {
+    // console.log(result);
+    if (result.show.image) {
+      const img = document.createElement("img");
+      img.src = result.show.image.medium;
+      document.body.append(img);
     }
-}
+  }
+};
